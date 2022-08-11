f0325afb354340579f1db9d52f601cd663e0d034
danielboyce <daarf92@gmail.com>
2022-08-11 11:07:29 +0100

    Next.js began. Using Bulma for styles too

---
+++ band-bounty/pages/BandBounty.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import 'bulma/css/bulma.css'
+import styles from '../styles/BandBounty.module.css'
+
+export default function BandBounty() {
+
+    return (
+        <div>
+        <Head>
+        <title>BandBounty</title>
+        <meta name="description" content="A blockchain based event app" />
+      </Head>
+
+      
+        </div>
+    )
+
+}
